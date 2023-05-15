72757cdeb84e4dc3d57203f15fdbe87dd24120bd
Drake Costa <drake@saeris.io>
2023-05-15 15:02:30 -0700

    remove unused files

--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { debounce } from "./debounce";
-export { injectProps } from "./injectProps";
-export { isRenderFunction } from "./isRenderFunction";
-export type { IsRenderFunction } from "./isRenderFunction";
-export { omit, pick, split } from "./split";
