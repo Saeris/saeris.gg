86e6600cf5402d5538ebcaa3951045b0914de08f
Drake Costa <drake@saeris.io>
2023-06-20 18:52:30 -0700

    add worbik link, update resume link

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-import { HiGlobeAmericas } from "react-icons/hi2";
 import {
   SiLinkedin,
   SiGmail,
@@ -7,12 +6,15 @@ import {
   SiGithub,
   SiSteam
 } from "react-icons/si";
+import { MdPermContactCalendar, MdGamepad } from "react-icons/md";
+
 import type { SiteConfig } from "./types";
 
 export const config: SiteConfig = {
   name: `Drake Costa`,
   title: `Engineering • Design • Photography`,
   links: [
+    { Icon: MdGamepad, url: `//worbik.saeris.io`, label: `Worbik` },
     { Icon: SiGithub, url: `//github.com/saeris`, label: `Github` },
     { Icon: SiTwitter, url: `//twitter.com/saeris`, label: `Twitter` },
     {
@@ -26,7 +28,7 @@ export const config: SiteConfig = {
       url: `//www.linkedin.com/in/saeris`,
       label: `LinkedIn`
     },
-    { Icon: HiGlobeAmericas, url: `//saeris.io`, label: `Resume` },
+    { Icon: MdPermContactCalendar, url: `//resume.saeris.gg`, label: `Resume` },
     { Icon: SiGmail, url: `mailto:yo@saer.is`, label: `Email` }
   ]
 };
